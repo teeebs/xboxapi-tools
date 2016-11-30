d4a9266bb163b62271d4a2fab0b98e3d67ba1c4f
Trevor Veralrud <trevor.veralrud@gmail.com>
2016-11-30 09:55:37 -0800

    Initial commit. Need to finish POST methods for sending messages and posting to activity feeds.

---
+++ pyxboxapi/__init__.py
@@ -0,0 +1,240 @@
+import requests
+import datetime
+
+
+class XboxApi(object):
+    def __init__(self, api_key=None, language=None, base_url=""):
+        self.api_key = api_key
+        if not self.api_key:
+            import os
+            self.api_key = os.environ.get("XBOXAPIKEY", None)
+        if not self.api_key:
+            raise ValueError("You must provide an XboxAPI.com API key or set XBOXAPIKEY environment variable")
+        self.language = language
+        self.base_url = base_url
+        if not self.base_url:
+            self.base_url = "https://xboxapi.com/v2"
+
+    def get(self, url):
+        """
+        Make a GET request to the Xbox API
+        :param url: Endpoint and parameters to append to self.base_url
+        :return: XboxApiResponse
+        """
+        # Build headers
+        headers = {"X-AUTH": self.api_key}
+        if self.language:
+            headers["Accept-Language"] = self.language
+        # Make request and build response
+        response = requests.get("{}{}".format(self.base_url, url), headers=headers)
+        return XboxApiResponse(self, url, response)
+
+    def get_profile(self):
+        """This is your profile information"""
+        return self.get("/profile")
+
+    def get_xuid(self):
+        """This is your account XUID (Xbox Account User ID)"""
+        return self.get("/accountXuid")
+
+    def get_messages(self):
+        """These are your message with full preview"""
+        return self.get("/messages")
+
+    def get_conversations(self):
+        """These are your conversations with full preview of the last message sent/received"""
+        return self.get("/conversations")
+
+    def get_xuid_by_gamertag(self, gamertag):
+        """This is the XUID for a specified Gamertag (Xbox Account User ID)"""
+        return self.get("/xuid/{}".format(gamertag))
+
+    def get_gamertag_by_xuid(self, xuid):
+        """This is the Gamertag for a specified XUID (Xbox Account User ID)"""
+        return self.get("/gamertag/{}".format(xuid))
+
+    def get_user_profile(self, xuid):
+        """This is the Profile for a specified XUID"""
+        return self.get("/{}/profile".format(xuid))
+
+    def get_user_gamercard(self, xuid):
+        """This is the Gamercard information for a specified XUID"""
+        return self.get("/{}/gamercard".format(xuid))
+
+    def get_user_presence(self, xuid):
+        """This is the current presence information for a specified XUID"""
+        return self.get("/{}/presence".format(xuid))
+
+    def get_user_activity(self, xuid):
+        """This is the current activity information for a specified XUID"""
+        return self.get("/{}/activity".format(xuid))
+
+    def get_user_activity_recent(self, xuid):
+        """This is the recent activity information for a specified XUID"""
+        return self.get("/{}/activity/recent".format(xuid))
+
+    def get_user_friends(self, xuid):
+        """This is the friends information for a specified XUID"""
+        return self.get("/{}/friends".format(xuid))
+
+    def get_user_followers(self, xuid):
+        """This is the followers information for a specified XUID"""
+        return self.get("/{}/followers".format(xuid))
+
+    def get_recent_players(self):
+        """This is accounts recent players information"""
+        return self.get("/recent-players")
+
+    def get_user_gameclips(self, xuid):
+        """This is the game clips for a specified XUID"""
+        return self.get("/{}/game-clips".format(xuid))
+
+    def get_user_saved_gameclips(self, xuid):
+        """This is the saved game clips for a specified XUID"""
+        return self.get("/{}/game-clips/saved".format(xuid))
+
+    def get_user_saved_gameclips_by_title(self, xuid, title_id):
+        """This is the saved game clips for a specified XUID, and Game (titleId)"""
+        return self.get("/{}/game-clips/{}".format(xuid, title_id))
+
+    def get_saved_gameclips(self, title_id):
+        """This is the saved game clips for a specified Game (titleId)"""
+        return self.get("/game-clips/{}".format(title_id))
+
+    def get_user_screenshots(self, xuid):
+        """This is the screenshots for a specified XUID"""
+        return self.get("/{}/screenshots".format(xuid))
+
+    def get_user_saved_screenshots(self, xuid, title_id):
+        """This is the saved screenshots for a specified XUID, and Game (titleId)"""
+        return self.get("/{}/screenshots/{}".format(xuid, title_id))
+
+    def get_saved_screenshots(self, title_id):
+        """This is the saved screenshots for a specified Game (titleId)"""
+        return self.get("/screenshots/{}".format(title_id))
+
+    def get_user_game_stats(self, xuid, title_id):
+        """This is the game stats for a specified XUID, on a specified game. (i.e. Driver Level on Forza etc.)"""
+        return self.get("/{}/game-stats/{}".format(xuid, title_id))
+
+    def get_user_xbox360games(self, xuid):
+        """This is the Xbox 360 Games List for a specified XUID"""
+        return self.get("/{}/xbox360games".format(xuid))
+
+    def get_user_xboxonegames(self, xuid):
+        """This is the Xbox One Games List for a specified XUID"""
+        return self.get("/{}/xboxonegames".format(xuid))
+
+    def get_user_achievements(self, xuid, title_id):
+        """This is the Xbox Games Achievements for a specified XUID"""
+        return self.get("/{}/achievements/{}".format(xuid, title_id))
+
+    def get_game_info_hex(self, game_id):
+        """This is the Xbox Game Information (using the game id in hex format)"""
+        return self.get("/game-details-hex/{}".format(game_id))
+
+    def get_game_info(self, product_id):
+        """This is the Xbox Game Information (using the product id)"""
+        return self.get("/game-details/{}".format(product_id))
+
+    def get_game_addons(self, product_id):
+        """This is the Xbox Game Information (using the product id)"""
+        return self.get("/game-details/{}/addons".format(product_id))
+
+    def get_game_related(self, product_id):
+        """This is the Xbox Game Information (using the product id)"""
+        return self.get("/game-details/{}/related".format(product_id))
+
+    def get_latest_xbox360games(self):
+        """This gets the latest Xbox 360 Games from the Xbox LIVE marketplace"""
+        return self.get("/latest-xbox360-games")
+
+    def get_latest_xboxonegames(self):
+        """This gets the latest Xbox One Games from the Xbox LIVE marketplace"""
+        return self.get("/latest-xboxone-games")
+
+    def get_latest_xboxoneapps(self):
+        """This gets the latest Xbox One Apps from the Xbox LIVE marketplace"""
+        return self.get("/latest-xboxone-apps")
+
+    def get_xboxone_gold(self):
+        """These are the free "Games with Gold", and "Deals with Gold" from the Xbox LIVE marketplace"""
+        return self.get("/xboxone-gold-lounge")
+
+    def get_xbox360games(self):
+        """Browse the Xbox LIVE marketplace for Xbox 360 content."""
+        return self.get("/browse-marketplace/xbox360/1?sort=releaseDate")
+
+    def get_xboxonegames(self):
+        """Browse the Xbox LIVE marketplace for Xbox One Game content."""
+        return self.get("/browse-marketplace/games/1?sort=releaseDate")
+
+    def get_xboxoneapps(self):
+        """Browse the Xbox LIVE marketplace for Xbox One App content."""
+        return self.get("/browse-marketplace/apps/1?sort=releaseDate")
+
+    def get_user_activity_feed(self):
+        """Show your activity feed."""
+        return self.get("/activity-feed")
+
+    def get_user_titlehub_achievements(self, xuid):
+        """Show your achievements list by game with friends who also play. (New TitleHub endpoint)"""
+        return self.get("/{}/titlehub-achievement-list".format(xuid))
+
+    def send_message(self):
+        pass
+
+    def send_activity_feed(self):
+        pass
+
+
+class XboxApiResponse(object):
+    def __init__(self, api, url, response):
+        self.api = api
+        self.url = url
+        self.response = response
+        try:
+            json = response.json()
+        except ValueError:
+            json = response.content
+        self.data = json
+        self._set_metadata(response)
+
+    def _set_metadata(self, response):
+        self.next_url = None
+        self.status_code = response.status_code
+        self.continuation_token = response.headers.get("X-Continuation-Token")
+        self.rate_limit = int(response.headers.get("X-RateLimit-Limit"))
+        self.rate_remaining = int(response.headers.get("X-RateLimit-Remaining"))
+        self.rate_reset = int(response.headers.get("X-RateLimit-Reset"))
+        self.rate_reset_datetime = datetime.datetime.now() + datetime.timedelta(seconds=self.rate_reset)
+        if self.continuation_token:
+            self.next_url = "{}{}continuationToken={}".format(self.url,
+                                                              "&" if "?" in self.url else "?",
+                                                              self.continuation_token)
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        if self.continuation_token:
+            return self.api.get(self.next_url)
+        else:
+            raise StopIteration()
+
+    def fill_data(self, maximum=None):
+        """
+        Make additional API calls and aggregate subsequent records up to `maximum` or
+        until no continuation token is returned.
+        :param maximum: Maximum number of records to return
+        :return: XboxApiResponse
+        """
+        while self.continuation_token and type(self.data) == list:
+            next_response = self.next()
+            # Merge new list with existing list
+            self.data.extend(next_response.data)
+            # Overwrite this responses metadata with latest
+            self._set_metadata(next_response.response)
+            if maximum and len(self.data) >= maximum:
+                self.data = self.data[:maximum]
+                break
---
+++ pyxboxapi/test_xboxapi.py
@@ -0,0 +1,136 @@
+import unittest
+from . import XboxApi
+
+gamertag = "Major Nelson"
+major_xuid = 2584878536129841
+title_id = 252034287  # Skyrim Special Edition
+xboxapi = XboxApi()
+me = xboxapi.get_xuid()
+my_gamertag, my_xuid = me.data["gamertag"], me.data["xuid"]
+
+
+class TestXboxApi(unittest.TestCase):
+    def setUp(self):
+        self.xboxapi = xboxapi
+        self.me = me
+
+    def test_get_profile(self):
+        self.assertTrue(type(self.xboxapi.get_profile()) == dict)
+
+    def test_get_profile(self):
+        self.assertEqual(self.xboxapi.get_profile().status_code, 200)
+
+    def test_get_xuid(self):
+        self.assertEqual(self.xboxapi.get_xuid().status_code, 200)
+
+    def test_get_messages(self):
+        self.assertEqual(self.xboxapi.get_messages().status_code, 200)
+
+    def test_get_conversations(self):
+        self.assertEqual(self.xboxapi.get_conversations().status_code, 200)
+
+    def test_get_xuid_by_gamertag(self):
+        self.assertEqual(self.xboxapi.get_xuid_by_gamertag(gamertag).status_code, 200)
+
+    def test_get_gamertag_by_xuid(self):
+        self.assertEqual(self.xboxapi.get_gamertag_by_xuid(major_xuid).status_code, 200)
+
+    def test_get_user_profile(self):
+        self.assertEqual(self.xboxapi.get_user_profile(major_xuid).status_code, 200)
+
+    def test_get_user_gamercard(self):
+        self.assertEqual(self.xboxapi.get_user_gamercard(major_xuid).status_code, 200)
+
+    def test_get_user_presence(self):
+        self.assertEqual(self.xboxapi.get_user_presence(major_xuid).status_code, 200)
+
+    def test_get_user_activity(self):
+        self.assertEqual(self.xboxapi.get_user_activity(major_xuid).status_code, 200)
+
+    def test_get_user_activity_recent(self):
+        self.assertEqual(self.xboxapi.get_user_activity_recent(major_xuid).status_code, 200)
+
+    def test_get_user_friends(self):
+        self.assertEqual(self.xboxapi.get_user_friends(major_xuid).status_code, 200)
+
+    def test_get_user_followers(self):
+        self.assertEqual(self.xboxapi.get_user_followers(my_xuid).status_code, 200)
+
+    def test_get_recent_players(self):
+        self.assertEqual(self.xboxapi.get_recent_players().status_code, 200)
+
+    def test_get_user_gameclips(self):
+        self.assertEqual(self.xboxapi.get_user_gameclips(major_xuid).status_code, 200)
+
+    def test_get_user_saved_gameclips(self):
+        self.assertEqual(self.xboxapi.get_user_saved_gameclips(major_xuid).status_code, 200)
+
+    def test_get_user_saved_gameclips_by_title(self):
+        self.assertEqual(self.xboxapi.get_user_saved_gameclips_by_title(major_xuid, title_id).status_code, 200)
+
+    def test_get_saved_gameclips(self):
+        self.assertEqual(self.xboxapi.get_saved_gameclips(title_id).status_code, 200)
+
+    def test_get_user_screenshots(self):
+        self.assertEqual(self.xboxapi.get_user_screenshots(major_xuid).status_code, 200)
+
+    def test_get_user_saved_screenshots(self):
+        self.assertEqual(self.xboxapi.get_user_saved_screenshots(major_xuid, title_id).status_code, 200)
+
+    def test_get_saved_screenshots(self):
+        self.assertEqual(self.xboxapi.get_saved_screenshots(title_id).status_code, 200)
+
+    def test_get_user_game_stats(self):
+        self.assertEqual(self.xboxapi.get_user_game_stats(major_xuid, title_id).status_code, 200)
+
+    def test_get_user_xbox360games(self):
+        self.assertEqual(self.xboxapi.get_user_xbox360games(major_xuid).status_code, 200)
+
+    def test_get_user_xboxonegames(self):
+        self.assertEqual(self.xboxapi.get_user_xboxonegames(major_xuid).status_code, 200)
+
+    def test_get_user_achievements(self):
+        self.assertEqual(self.xboxapi.get_user_achievements(major_xuid, title_id).status_code, 200)
+
+    def test_get_game_info_hex(self):
+        self.assertEqual(self.xboxapi.get_game_info_hex().status_code, 200)
+
+    def test_get_game_info(self):
+        self.assertEqual(self.xboxapi.get_game_info().status_code, 200)
+
+    def test_get_game_addons(self):
+        self.assertEqual(self.xboxapi.get_game_addons().status_code, 200)
+
+    def test_get_game_related(self):
+        self.assertEqual(self.xboxapi.get_game_related().status_code, 200)
+
+    def test_get_latest_xbox360games(self):
+        self.assertEqual(self.xboxapi.get_latest_xbox360games().status_code, 200)
+
+    def test_get_latest_xboxonegames(self):
+        self.assertEqual(self.xboxapi.get_latest_xboxonegames().status_code, 200)
+
+    def test_get_latest_xboxoneapps(self):
+        self.assertEqual(self.xboxapi.get_latest_xboxoneapps().status_code, 200)
+
+    def test_get_xboxone_gold(self):
+        self.assertEqual(self.xboxapi.get_xboxone_gold().status_code, 200)
+
+    def test_get_xbox360games(self):
+        self.assertEqual(self.xboxapi.get_xbox360games().status_code, 200)
+
+    def test_get_xboxonegames(self):
+        self.assertEqual(self.xboxapi.get_xboxonegames().status_code, 200)
+
+    def test_get_xboxoneapps(self):
+        self.assertEqual(self.xboxapi.get_xboxoneapps().status_code, 200)
+
+    def test_get_user_activity_feed(self):
+        self.assertEqual(self.xboxapi.get_user_activity_feed().status_code, 200)
+
+    def test_get_user_titlehub_achievements(self):
+        self.assertEqual(self.xboxapi.get_user_titlehub_achievements(major_xuid).status_code, 200)
+
+
+if __name__ == "__main__":
+    unittest.main()
